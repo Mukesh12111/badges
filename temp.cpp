bdf6627e362944c3293a606d3d3f5912a148fc67
MOHAMMAD FAROOQ <66942957+FAROOQkhan10@users.noreply.github.com>
2022-11-21 22:14:19 +0530

    Create temp.cpp
    Added c++ template

---
+++ temp.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+ 
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  
+ 
+ 
+   int t; cin>>t;
+   while(t--){
+      
+      
+        cout<<"farooq khan\n";
+	  
+     }
+}
+
